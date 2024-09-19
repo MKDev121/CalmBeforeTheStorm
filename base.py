a0da511983299e28186912c5aa2651ea61b6492e
MKDev <58660200+MKDev121@users.noreply.github.com>
2024-09-19 18:43:12 +0530

    Reorganising the project

---
+++ base.py
@@ -0,0 +1,7 @@
+"""A script containing base class and inventory class"""
+
+class Base:
+    """Object which is the main control unit in the game, which we have to defend from enemies"""
+    def __init__(self,position):
+        self.position=position
+        
